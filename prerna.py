421076f0e50b45e18629978505e7abb5cf704730
PrernaBagul <prernacoding09@gmail.com>
2025-08-08 08:36:39 +0530

    new code

--- prerna.py
+++ prerna.py
@@ -1,9 +1,11 @@
-# Define a float
-y = 1.
-print(y)
-print(type(y))
 
-# Convert float to integer with the int function
-z = int(y)
-print(z)
-print(type(z))
+
+# Ask for user input
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+
+# Calculate next year's age
+next_year_age = age + 1
+
+# Display the message
+print(f"Hello, {name}! Next year, you will be {next_year_age} years old.")
