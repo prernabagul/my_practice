681799c21d5f069ea691d67ec9ce2e7c7beb6a90
PrernaBagul <prernacoding09@gmail.com>
2025-08-08 08:40:01 +0530

    updated code

--- prerna.py
+++ prerna.py
@@ -9,3 +9,8 @@ next_year_age = age + 1
 
 # Display the message
 print(f"Hello, {name}! Next year, you will be {next_year_age} years old.")
+
+print(3 * True)
+print(-3.1 * True)
+print(type("abc" * False))
+print(len("abc" * False))
