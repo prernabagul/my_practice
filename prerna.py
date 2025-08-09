3f42a2151b68ebc7e436ac8348e75f07578db39e
PrernaBagul <prernacoding09@gmail.com>
2025-08-09 20:24:03 +0530

    new code

--- prerna.py
+++ prerna.py
@@ -1,16 +1,5 @@
-
-
-# Ask for user input
-name = input("What's your name? ")
-age = int(input("How old are you? "))
-
-# Calculate next year's age
-next_year_age = age + 1
-
-# Display the message
-print(f"Hello, {name}! Next year, you will be {next_year_age} years old.")
-
-print(3 * True)
-print(-3.1 * True)
-print(type("abc" * False))
-print(len("abc" * False))
+print(False + False)
+print(True + False)
+print(False + True)
+print(True + True)
+print(False + True + True + True)
