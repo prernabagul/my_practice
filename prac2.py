aeafd2b7e6d3b460d4288fd74bf4a55a73c79568
PrernaBagul <prernacoding09@gmail.com>
2026-01-12 21:06:20 +0530

    initial commit

--- prac2.py
+++
@@ -1,6 +0,0 @@
-item = input("what would you like to buy: ")
-prize = float(input("what is the prize: "))
-quantity = int(input("how many would you like: "))
-
-total = prize * quantity
-print(f"your total is: {total}")
