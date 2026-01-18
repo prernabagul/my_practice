ec884c98d5b0ead5e53f5abdc014ab2c80b5807e
PrernaBagul <prernacoding09@gmail.com>
2026-01-18 21:37:39 +0530

    update

--- tpprac.py
+++ tpprac.py
@@ -15,3 +15,14 @@ topper = max(students, key=students.get)
 
 print("Average Marks:", average)
 print("Topper:", topper, "with", students[topper], "marks")
+
+
+# list_stats.py
+
+numbers = [10, 20, 30, 40, 50]
+
+print("Max:", max(numbers))
+print("Min:", min(numbers))
+print("Sum:", sum(numbers))
+print("Average:", sum(numbers) / len(numbers))
+
