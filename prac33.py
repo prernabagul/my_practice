9bd3308794c107fe859a454be227d78f27a23962
PrernaBagul <prernacoding09@gmail.com>
2026-01-19 22:04:23 +0530

    udated comit 2

--- prac33.py
+++ prac33.py
@@ -29,3 +29,18 @@ for email in emails:
         print(email, "is valid")
     else:
         print(email, "is invalid")
+
+
+# duplicate_finder.py
+
+numbers = [1, 2, 3, 2, 4, 5, 1, 6]
+
+duplicates = set()
+seen = set()
+
+for num in numbers:
+    if num in seen:
+        duplicates.add(num)
+    seen.add(num)
+
+print("Duplicates:", duplicates)
