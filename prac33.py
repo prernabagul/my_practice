960e9f4369e6baa2140da1742e4744b340c5da8f
PrernaBagul <prernacoding09@gmail.com>
2026-01-20 22:05:30 +0530

    udateddd commmitt

--- prac33.py
+++ prac33.py
@@ -55,4 +55,17 @@ print("Current Date:", now.date())
 print("Current Time:", now.time())
 print("Year:", now.year)
 
+# prime_checker.py
 
+numbers = [2, 3, 4, 5, 10, 11, 13]
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+for num in numbers:
+    print(num, "Prime" if is_prime(num) else "Not Prime")
